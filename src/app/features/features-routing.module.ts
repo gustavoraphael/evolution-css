d3dc3526690922c25d672d1704ee96bdbd81e882
Gustavo Raphael <gustavo.raphael@totvs.com.br>
2021-09-10 12:22:18 -0300

    commit register

---
+++ src/app/features/features-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  //{ path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  //{ path: 'register-1', component: RegisterComponent },
+  
+  {
+    path: '**', redirectTo: ''
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class FeaturesRoutingModule { }
